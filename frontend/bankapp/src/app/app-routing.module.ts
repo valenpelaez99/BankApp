c9786c09c1464c372d69968fd5e9cbae6f9f420a
valenpelaez99 <valenpelaez99@gmail.com>
2023-01-13 00:55:01 -0500

    Routing module config

--- frontend/bankapp/src/app/app-routing.module.ts
+++ frontend/bankapp/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountsComponent } from './components/accounts/accounts.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { HomeComponent } from './components/home/home.component';
+import { InfoComponent } from './components/info/info.component';
+import { TransactionsComponent } from './components/transactions/transactions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'return', redirectTo: '/accounts/:id'},
+  {path: 'home', component: HomeComponent},
+  {path: 'clients', component: ClientsComponent},
+  {path: 'accounts/:idClient', component: AccountsComponent},
+  {path: 'transactions/:idAccount', component: TransactionsComponent},
+  {path: 'developer', component: InfoComponent},
+  {path: '**', pathMatch: 'full', redirectTo:'/home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/bankapp/src/app/components/home/home.component.ts
+++ frontend/bankapp/src/app/components/home/home.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  displayPosition: boolean = false;
+  position: string = '';
+  responsiveOptions;
+
+  images: string[] = ["/assets/2.png","/assets/6.jpeg","/assets/3.png","/assets/4.jpg","/assets/5.jpg","/assets/7.jpg","/assets/1.png"];
+
+  constructor() {
+    this.responsiveOptions = [
+      {
+        breakpoint: '1024px',
+        numVisible: 3,
+        numScroll: 3,
+      },
+      {
+        breakpoint: '768px',
+        numVisible: 2,
+        numScroll: 2,
+      },
+      {
+        breakpoint: '560px',
+        numVisible: 1,
+        numScroll: 1,
+      },
+    ];
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
