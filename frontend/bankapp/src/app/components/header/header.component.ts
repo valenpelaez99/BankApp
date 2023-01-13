2e979f2ccc1b08ae54ba1e9044a04d50030c9350
valenpelaez99 <valenpelaez99@gmail.com>
2023-01-13 01:01:51 -0500

    Home and Header components updated

--- frontend/bankapp/src/app/components/header/header.component.ts
+++ frontend/bankapp/src/app/components/header/header.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  goToHome(){
+    this.router.navigate(['home']);
+  }
+
+  goToClients(){
+    this.router.navigate(['clients']);
+  }
+
+  goToProfile(){
+    this.router.navigate(['info']);
+  }
 
 }
